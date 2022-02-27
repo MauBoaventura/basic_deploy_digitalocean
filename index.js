bcc7cb3b08b1b22874ec654d8a08aa4296cfe090
Mauboa <mba097@gmail.com>
2022-02-27 05:36:51 -0300

    Enable CI

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 // Respond with "hello world" for requests that hit our root "/"
 app.get("/", function (req, res) {
- return res.send("Hello Worlld");
+ return res.send("Hello Mauboa");
 });
 // listen to port 3000 by default
 app.listen(process.env.PORT || 3000, () => {
