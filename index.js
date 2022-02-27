b30eeaeec99fd71cbbd7218f83d608ed12b21df6
Mauboa <mba097@gmail.com>
2022-02-27 06:16:12 -0300

    Enable CI

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get("/", function (req, res) {
  return res.send("Hello Mauboa");
 });
 // listen to port 3000 by default
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3001, () => {
   console.log("Server is running");
 });
 module.exports = app;
